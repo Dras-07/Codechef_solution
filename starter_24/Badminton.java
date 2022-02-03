56898f9ea42f5d704aa55b1a6203a3ca1158f076
Anjanay Singh <63287310+Dras-07@users.noreply.github.com>
2022-02-03 20:02:59 +0530

    Create Badminton.java

---
+++ starter_24/Badminton.java
@@ -0,0 +1,17 @@
+
+
+import java.util.*;
+
+class Codechef{
+    public static void main(String[] args)throws Exception{
+        try (Scanner sc = new Scanner(System.in)) {
+            int t=sc.nextInt();
+            while(t-->0)
+            {
+                int n=sc.nextInt();
+                int k=n/2+1;
+                System.out.println(k);
+            }
+        }
+    }
+}
