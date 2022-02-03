48d0decb2afb80c0ce613b87d3c9b279a0f1981e
Anjanay Singh <63287310+Dras-07@users.noreply.github.com>
2022-02-03 20:04:41 +0530

    Create ORANDCON.java

---
+++ starter_24/ORANDCON.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+class Codechef{
+    public static void main(String[] args)throws Exception{
+        try (Scanner sc = new Scanner(System.in)) {
+            int t=sc.nextInt();
+            while(t-->0)
+            {
+               long x=sc.nextLong();
+               long g=134217727;
+                //int max=Math.max(p,Math.max(q,r));
+                     System.out.println(0+" "+g+" "+(x));
+              
+                
+            }
+        }
+    }
+}
